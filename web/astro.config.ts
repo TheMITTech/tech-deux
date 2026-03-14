bbac0927208b35acbba177ce5c16b4290b816662
Colin C <contact@colclark.net>
2026-03-14 02:15:32 -0400

    added web scaffolding, base css styles

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from "astro/config";
-
 import node from "@astrojs/node";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
 });
